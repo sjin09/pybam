e2a43e66246de4f2b533ec72ed4856e5d9cfd602
Sangjin <sl17@sanger.ac.uk>
2021-05-06 10:41:17 +0100

    pybam:main

--- src/pybam/__main__.py
+++ src/pybam/__main__.py
@@ -1,12 +1,28 @@
-"""Command-line interface."""
-import click
+#!/usr/bin/env python3
+__version__ = "0.0.1"
+__author__ = "Sangjin Lee"
 
 
-@click.command()
-@click.version_option()
-def main() -> None:
-    """pybam."""
+# modules
+import os
+import sys
+import logging
+from pybam.fastq import bam2fastq
+from pybam.parse_args import parse_args
 
+def main():
+    parser, options = parse_args(program_version=__version__)
+
+    if options.sub == "bam2fastq":  # return first n lines of sequences
+        bam2fastq(options.input, options.output)
+    # elif options.sub == "filter": ## return first n lines of sequences
+    #     hard_filter(options.input, options.number, options.output)
+    # elif options.sub == "tricounts": ## returns counts based on trinucleotide context
+    #     seq_statistics(options.input, options.ref, options.output)
+    else:
+        logging.warning("The subcommand does not exist!\n")
+        parser.print_help()
 
 if __name__ == "__main__":
-    main(prog_name="pybam")  # pragma: no cover
+    main()
+    sys.exit(0)
